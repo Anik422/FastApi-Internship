8d9659b3d2d0bc192ce683679f4e8e21d540cbc4
Anik Saha <aniks422@gmail.com>
2024-08-15 18:02:07 +0600

    using routers

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from enum import Enum
-from fastapi import FastAPI, status, Response
-from typing import Optional
-
+from fastapi import FastAPI, status
+from routers import blog_get
 
 app  = FastAPI()
+
+app.include_router(blog_get.router)
+
 
 
 
@@ -16,68 +17,3 @@ app  = FastAPI()
          ) 
 def index():
     return {"message": "Hello, World"}
-
-# path parameters order by value type
-# @app.get("/blog/all")
-# def get_all_blogs():
-#     return {"message": "All blogs provided"}
-
-#Query Parameters
-@app.get("/blog/all", 
-            status_code=status.HTTP_200_OK, 
-            tags=['Blog'],
-            summary="Get all blogs",
-            description="This API returns all blogs",
-            response_description="All blogs response"
-         ) 
-def get_all_blogs(page = 1, page_size : Optional[int] = None):
-    return {"message": f"All {page_size} blogs on page {page}"}
-
-
-@app.get("/blog/{id}/comments/{comment_id}", 
-            status_code=status.HTTP_200_OK, 
-            tags=['Blog', 'Comment'],
-            response_description="Blog comment response",
-    )
-def get_comment(id:int, comment_id:int, valid:bool = True, username: Optional[str] = None):
-    """
-    This API returns comment for a blog
-
-    - **id**: blog id
-    - **comment_id**: comment id
-    - **valid**: valid comment
-    - **username**: username
-
-    """
-    return {"message": f"blog_id {id}, comment_id {comment_id}, valid {valid}, username: {username}"}
-
-# Enum class 
-class BlogType(str, Enum):
-    short = "Short"
-    story = "Story" 
-    howto = "Howto"
-#predefined value
-@app.get("/blog/type/{type}", 
-            status_code=status.HTTP_200_OK, 
-            tags=['Blog'],
-            summary="Get blog type",
-            description="This API returns blog type",
-            response_description="Blog type response"
-        ) 
-def get_blog_type(type : BlogType):
-    return {"message": f"Blog type {type}"}
-
-# path parameters
-@app.get('/blog/{id}', 
-            status_code=status.HTTP_200_OK, 
-            tags=['Blog'],
-            summary="Get blog by id",
-            description="This API returns blog by id",
-            response_description="Blog by id response"
-        ) 
-def get_blog(id:int, response: Response):
-    if id > 5:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {"message": "Blog {id} not found"}
-    response.status_code = status.HTTP_200_OK
-    return {"message": f"Blog with id {id}"}
---
+++ routers/blog_get.py
@@ -0,0 +1,77 @@
+from fastapi import APIRouter, status, Response
+from typing import Optional
+from enum import Enum
+
+
+
+router = APIRouter(
+    prefix="/blog",
+    tags=['Blog']
+)
+
+
+
+
+#Query Parameters
+@router.get("/all", 
+            status_code=status.HTTP_200_OK, 
+            summary="Get all blogs",
+            description="This API returns all blogs",
+            response_description="All blogs response"
+         ) 
+def get_all_blogs(page = 1, page_size : Optional[int] = None):
+    return {"message": f"All {page_size} blogs on page {page}"}
+
+
+
+
+@router.get("/{id}/comments/{comment_id}", 
+            status_code=status.HTTP_200_OK, 
+            tags=['Comment'],
+            response_description="Blog comment response",
+    )
+def get_comment(id:int, comment_id:int, valid:bool = True, username: Optional[str] = None):
+    """
+    This API returns comment for a blog
+
+    - **id**: blog id
+    - **comment_id**: comment id
+    - **valid**: valid comment
+    - **username**: username
+
+    """
+    return {"message": f"blog_id {id}, comment_id {comment_id}, valid {valid}, username: {username}"}
+
+
+
+# Enum class 
+class BlogType(str, Enum):
+    short = "Short"
+    story = "Story" 
+    howto = "Howto"
+#predefined value
+@router.get("/type/{type}", 
+            status_code=status.HTTP_200_OK, 
+            summary="Get blog type",
+            description="This API returns blog type",
+            response_description="Blog type response"
+        ) 
+def get_blog_type(type : BlogType):
+    return {"message": f"Blog type {type}"}
+
+
+
+
+# path parameters
+@router.get('/{id}', 
+            status_code=status.HTTP_200_OK, 
+            summary="Get blog by id",
+            description="This API returns blog by id",
+            response_description="Blog by id response"
+        ) 
+def get_blog(id:int, response: Response):
+    if id > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"message": "Blog {id} not found"}
+    response.status_code = status.HTTP_200_OK
+    return {"message": f"Blog with id {id}"}
