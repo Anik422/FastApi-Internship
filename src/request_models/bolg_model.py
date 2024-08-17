6d382182b98833ebea8194cf3b5fceb0c9b40c6e
Anik Saha <aniks422@gmail.com>
2024-08-17 14:36:56 +0600

    Parameters done

--- src/request_models/bolg_model.py
+++ src/request_models/bolg_model.py
@@ -1,10 +1,17 @@
-from typing import Optional
+from typing import Optional, List, Dict
 from pydantic import BaseModel
 
 
+
+class Image(BaseModel):
+    url: str
+    alias: str
 
 class BlogModel(BaseModel):
     title: str
     content: str
     nb_comments: int
     published: Optional[bool] 
+    tags: List[str] = []
+    metadata: Dict[str, str] = {'key1': 'val1'}
+    image: Optional[Image] = None
--- src/routers/blog_post.py
+++ src/routers/blog_post.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, status, Query, Body
-from typing import Optional
+from fastapi import APIRouter, status, Query, Body, Path
+from typing import Optional, List, Dict
 from  request_models.bolg_model import BlogModel
 
 
@@ -24,24 +24,42 @@ def create_blog(blog: BlogModel, id:int, version: int = 1):
 
 @router.post("/new/{id}/comment", status_code=status.HTTP_201_CREATED)
 def create_comment(blog: BlogModel, id:int, 
-                   commnt_id = Query(None, 
-                                   title="Comment ID", 
-                                    description="This is the comment ID", 
-                                    alias="commentId", 
-                                    deprecated=True
+                   commnt_title = Query(None, 
+                                        min_length=5, 
+                                        max_length=50, 
+                                        title="Comment Title",
+                                        description="Title of the comment",
+                                        alias="commentTitle",
+                                        deprecated=True
                                     ),
                     content: str =  Body(..., 
                                          min_length=10, 
                                          max_length=100,
-                                         regex="^[a-z\s]*$",
-                                         )                 
-                                    
+                                        
+                                        ),              
+                    v: Optional[List[str]] = Query(["1.0", "1.1", "1.2"],),
+                    commnt_id: Optional[int] = Path(..., 
+                                                    title="Comment Id", 
+                                                    description="Id of the comment",
+                                                    ge=1,
+                                                    le=100,
+                                                    )               
                     ):
     """
     This API creates a new comment
     - **blog**: blog object
     - **id**: blog id
+    - **commnt_title**: comment title
     - **commentId**: comment id
+    - **content**: comment content
+    - **v**: version 
     """
 
-    return {"id" : id, "blog" : blog, "comment_id" : commnt_id, "content" : content, "message" : "Comment created successfully"}
+    return {"id" : id, 
+            "comment_title" : commnt_title,
+            "blog" : blog, 
+            "comment_id" : commnt_id, 
+            "content" : content, 
+            "version" : v,
+            "message" : "Comment created successfully"
+            }
