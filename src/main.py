7683244ef5e2f43f601a4c95f12ddeca79b5da35
Anik Saha <aniks422@gmail.com>
2024-08-18 17:17:47 +0600

    file upload, read, download done

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, status
-from routers import blog_get, product, blog_post, user, article
+from routers import blog_get, product, blog_post, user, article, file
 from auth import authentication
 from db import models
 from db.database import engine
@@ -7,6 +7,7 @@ from exceptions.stroy_exceptions import StroryException
 from fastapi.responses import JSONResponse
 from fastapi import Request, status
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 app  = FastAPI()
 app.include_router(authentication.router)
@@ -15,6 +16,7 @@ app.include_router(blog_post.router)
 app.include_router(user.router)
 app.include_router(article.router) 
 app.include_router(product.router)
+app.include_router(file.router)
 
 
 
@@ -56,3 +58,5 @@ app.add_middleware(
     allow_methods = ["*"],
     allow_headers = ["*"]
 )
+
+app.mount("/files", StaticFiles(directory="files"), name="files")
